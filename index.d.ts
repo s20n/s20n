edde476c754d772528d8f7a14d78656aa5637340
Marr11317 <remimarche@gmail.com>
2021-03-28 14:20:31 -0400

    fix types

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,33 @@
 declare module "s20n" {
     import { SvelteComponentTyped } from 'svelte';
 
+    /** The props that can be passed to the S20n component. */
     export interface S20nProps {
         /** An array of locales to load asynchronously. Note that they will all be loaded at once. */
-        locales: string;
-        /** Also default loaded language. */
+        locales: localeType[];
+        /** Default loaded language. */
         current?: string;
     }
     /** The S20n component provides internationalization for a Svelte app. */
-    export class S20n extends SvelteComponentTyped<S20nProps> {}
+    export default class S20n extends SvelteComponentTyped<S20nProps> { }
 
     /** Describes a locale with the path to its json file and its name. */
     export type localeType = {path: string, name: string};
 
-    import { writable, derived } from 'svelte/store';
+    import { Writable, Readable } from 'svelte/store';
 
     /** The locale to fallback to if the current locale doesn't contain the requested translation. */
-    export const fallbackLocale = writable<string>("en");
+    export const fallbackLocale: Writable<string>;
     /** The currently displayed locale. */
-    export const locale = writable<string>("Loading");
+    export const locale: Writable<string>;
 
     /** The type of the translate function. */
     type translateFunctionType = (path: string, defaultValue?: string) => string;
     /** The translate function. */
-    export const t = derived<ReturnType<typeof writable>, translateFunctionType>(locale, () => {
-        return function (path: string, defaultValue?: string): string {
-            return getTranslation(path, defaultValue);
-        }
-    });
+    export const t: Readable<string>;
 
     /** alias for t (translate) */
-    export const _ = t;
+    export const _: typeof t;
     /** alias for t */
-    export const translate = t;
+    export const translate: typeof t;
 }
