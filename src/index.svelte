9849d513fa91e5b7a602ecc32e17fd064a59cf79
Marr11317 <remimarche@gmail.com>
2021-04-01 17:42:14 -0400

    remove docs from source

--- src/index.svelte
+++ src/index.svelte
@@ -2,27 +2,17 @@
     import { writable, get, derived } from 'svelte/store';
     import marked from "marked";
 
-    /** The locale to fallback to if the current locale doesn't contain the requested translation. */
     export const fallbackLocale = writable<string>("en");
-    /** The currently displayed locale. */
     export const locale = writable<string>("Loading");
 
-    /** The options that can be passed to the translate function */
     export interface TranslationParams {
-        /**
-         * Use [marked](https://marked.js.org).
-         * Note that you need the `@html` tag in order for the html to be rendered correctly.
-         */
         useMarkdown: boolean;
     }
-    /** The default options passed to the translation function. Can be overwritten by setting the `params` argument. */
     export const defaultTranslationParams = writable<TranslationParams>({ useMarkdown: false })
 
     const _loadedLocales = {};
 
-    /** The type of the translate function. */
     type translateFunctionType = (path: string, defaultValue?: string, params?: TranslationParams) => string;
-    /** The translate function. */
     export const t = derived<ReturnType<typeof writable>, translateFunctionType>(locale, () => {
         return function (path: string, defaultValue?: string, params?: TranslationParams): string {
             let p: TranslationParams = Object.assign({}, get(defaultTranslationParams));
@@ -33,9 +23,7 @@
             return getTranslation(path, defaultValue);
         }
     });
-    /** alias for t (translate) */
     export const _ = t;
-    /** alias for t */
     export const translate = t;
 
     function getTranslation(path: string, defaultValue?: string) {
@@ -69,7 +57,6 @@
         return null;
     }
 
-    /** Describes a locale with the path to its json file and its name. */
     export type localeType = {path: string, name: string};
 </script>
 
@@ -77,10 +64,8 @@
     import { onMount } from "svelte";
     import JSON5 from "json5";
 
-    /** An array of locales to load asynchronously. Note that they will all be loaded at once. */
     export let locales: localeType[];
 
-    /** Default loaded language. */
     export let current: string = "en";
 
     onMount(() => {
@@ -106,8 +91,3 @@
         }
     });
 </script>
-
-<!--
-@component
-The S20n component provides internationalization for a Svelte app.
--->
